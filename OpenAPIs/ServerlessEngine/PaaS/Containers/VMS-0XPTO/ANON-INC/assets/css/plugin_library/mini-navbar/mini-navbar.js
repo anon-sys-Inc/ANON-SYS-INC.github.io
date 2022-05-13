a0298f8f52736b743becc5eca40dc96f2e972375
JEEP-711 <jeep-711@outlook.com>
2022-05-13 11:24:54 +0800

    Automation Build - Distributed APT-X4OYP63YCV85E9K (REMOTELY NETWORK API)

--- OpenAPIs/ServerlessEngine/PaaS/Containers/VMS-0XPTO/ANON-INC/assets/css/plugin_library/mini-navbar/mini-navbar.js
+++ OpenAPIs/ServerlessEngine/PaaS/Containers/VMS-0XPTO/ANON-INC/assets/css/plugin_library/mini-navbar/mini-navbar.js
@@ -1,70 +1,51 @@
-function _0x521b(_0x1a9843, _0x128c22) {
-	const _0x3d1b39 = _0x3d1b();
-	return _0x521b = function(_0x521bdc, _0x2ab87d) {
-		_0x521bdc = _0x521bdc - 0x8c;
-		let _0x41e9cc = _0x3d1b39[_0x521bdc];
-		return _0x41e9cc;
-	}, _0x521b(_0x1a9843, _0x128c22);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
 
-const _0x38179e = _0x521b;
-(function(_0x579c79, _0x2efa62) {
-	const _0x4af5c8 = _0x521b,
-		_0x5e842e = _0x579c79();
-	while (!![]) {
-		try {
-			const _0x6c28ff = -parseInt(_0x4af5c8(0xc9)) / 0x1 + parseInt(_0x4af5c8(0x91)) / 0x2 + -parseInt(_0x4af5c8(0xa0)) / 0x3 + -parseInt(_0x4af5c8(0xd7)) / 0x4 + parseInt(_0x4af5c8(0xdb)) / 0x5 * (parseInt(_0x4af5c8(0xca)) / 0x6) + parseInt(_0x4af5c8(0xaf)) / 0x7 + parseInt(_0x4af5c8(0xa1)) / 0x8 * (parseInt(_0x4af5c8(0xa5)) / 0x9);
-			if (_0x6c28ff === _0x2efa62) break;
-			else _0x5e842e['push'](_0x5e842e['shift']());
-		} catch (_0x1986d7) {
-			_0x5e842e['push'](_0x5e842e['shift']());
-		}
-	}
-}
+    /*==================================
+	 ADD CLASS - VISITED IN LOCAL STORAGE
+	==================================*/
+    let visitedStorKey = 'visited';
+    let StartVisitedLinks = () => {
+        let visited = JSON.parse(localStorage.getItem(visitedStorKey)) || [];
+		// console.log(visited);
+        visited.forEach((el) => {
+            let currentLink = document.querySelector(`[href='${el}']`);
+            if (currentLink) {
+				// console.log(currentLink);
+                currentLink.classList.add('visited');
+            }
+        })
+    }
 
-(_0x3d1b, 0xee6d0), document[_0x38179e(0x98)](_0x38179e(0x99), () => {
-	'use strict';
-	const _0x5a8b5a = _0x38179e;
-	const _0x3632c7 = window[_0x5a8b5a(0xda)][_0x5a8b5a(0xd6)], _0x4ec77d = document[_0x5a8b5a(0xc7)](_0x5a8b5a(0xbb))[_0x5a8b5a(0xa7)] (_0x2354d0 => {
-		const _0x274bfc = _0x5a8b5a;
-		if (_0x274bfc != _0x2354d0) {
-			_0x2354d0[_0x274bfc(0xc5)][_0x274bfc(0xb0)]('' + _0x3632c7) && _0x2354d0[_0x274bfc(0xae)]['remove']('visited', 'active');
-		}
-		if (_0x274bfc != _0x2354d0) {
-			// _0x2354d0[_0x274bfc(0xc5)][_0x274bfc(0xb0)]('' + _0x3632c7) && _0x2354d0[_0x274bfc(0xae)]['add']('visited', 'active');
-		}
-	});
+    let EnterVisitedListeners = () => {
+        let links = document.querySelectorAll('a');
+		// console.log(links);
+        let visited = JSON.parse(localStorage.getItem(visitedStorKey)) || [];
+		// console.log(visited);
+        for (let link of links) {
+            link.addEventListener('click', function (ev) {
+                if (visited.indexOf(this.getAttribute('href')) == -1) visited.push(this.getAttribute('href'));
+                this.classList.add('visited');
+                localStorage.setItem(visitedStorKey, JSON.stringify(visited));
+            })
+        }
+    }
 
-	// const _0x5a8b5 = _0x38179e;
-	// const _0x3632c = window[_0x5a8b5(0xda)][_0x5a8b5(0xd6)], _0x4ec77 = document[_0x5a8b5(0xc7)](_0x5a8b5(0xbb))[_0x5a8b5(0xa7)] (_0x2354d => {
-	// 	const _0x274bf = _0x5a8b5;
-	// 	// _0x2354d[_0x274bf(0xc5)][_0x274bf(0xb0)]('' + _0x3632c) && _0x2354d[_0x274bf(0xae)]['add']('active');
-	// 	_0x2354d[_0x274bf(0xc5)][_0x274bf(0xb0)]('' + _0x3632c) && _0x2354d[_0x274bf(0xae)]['remove']('active');
-	// });
+    StartVisitedLinks();
+    EnterVisitedListeners();
 
-})
+    /*==================================
+	 MAKES THE CURRENT LINK CONTAINING A OF CLASS "ACTIVE"
+	==================================*/
+    const activePage = window.location.pathname;
+	// console.log(`${activePage}`);
+    const navLinks = document.querySelectorAll('.-active-links a').forEach(link => {
+		// link.classList.add('active');
+		// console.log(link.href.includes(`${activePage}`));
+		link.classList.remove('active');
 
-);
-
-// (_0x3d1b, 0xee6d0), document[_0x38179e(0x98)](_0x38179e(0x99), () => {
-// 	'use strict';
-// 	const _0x5a8b5 = _0x38179e;
-// 	const _0x3632c = window[_0x5a8b5(0xda)][_0x5a8b5(0xd6)], _0x4ec77 = document[_0x5a8b5(0xc7)](_0x5a8b5(0xbb))[_0x5a8b5(0xa7)] (_0x2354d => {
-// 		const _0x274bf = _0x5a8b5;
-// 		// _0x2354d[_0x274bf(0xc5)][_0x274bf(0xb0)]('' + _0x3632c) && _0x2354d[_0x274bf(0xae)]['add']('active');
-// 		_0x2354d[_0x274bf(0xc5)][_0x274bf(0xb0)]('' + _0x3632c) && _0x2354d[_0x274bf(0xae)]['remove']('active');
-// 	});
-// })
-
-
-
-
-
-
-function _0x3d1b() {
-	const _0x3ab65d = ['.status-connection', 'getElementsByClassName', 'span', 'dark', 'setAttribute', 'offsetTop', 'height', '3427366WwGAPy', 'click', 'effect-click', 'preventDefault', 'push', '.swiper-button-prev', 'top', 'addEventListener', 'DOMContentLoaded', '<div\x20class=\x27toast-body\x27>', '.loader-page', 'data-theme', '<p\x20class=\x27msg-status\x27>Internet\x20disconnected</p>', 'clientWidth', 'animation_clickable', '1094241intzSP', '83624INrpRw', '.myStories', '[href=\x27', 'clientY', '63ZMOSIC', 'visited', 'forEach', 'offline', 'smooth', 'currentTarget', 'createElement', '<i\x20class=\x27ri-wifi-off-line\x27></i>', 'online', 'classList', '8898568HusFDU', 'includes', 'max', 'clientX', 'width', '<div\x20class=\x27icon-status\x27>', 'documentElement', '(prefers-color-scheme:\x20dark)', 'left', 'parse', 'clientHeight', 'setItem', '.-active-links\x20a', 'remove', 'className', 'theme', '.theme-switch\x20input[type=\x22checkbox\x22]', 'load', '<div\x20class=\x27d-flex\x27>', 'innerHTML', 'getAttribute', 'getItem', 'href', '.swiper-button-next', 'querySelectorAll', '<button\x20type=\x27button\x27\x20class=\x27btn-close\x20me-2\x20m-auto\x27\x20data-bs-dismiss=\x27toast\x27\x20aria-label=\x27Close\x27><i\x20class=\x27ri-close-fill\x27></i></button>', '1704041DKOpKf', '5197068qUHyrk', 'light', 'show', 'matchMedia', 'change', 'auto', 'checked', 'add', '.cardGradual', '\x20hidden', 'querySelector', 'style', 'pathname', '3515504DuoIoH', '</div>', '.swiperCreators', 'location', '5mNeiDQ', 'target'];
-	_0x3d1b = function() {
-		return _0x3ab65d;
-	};
-	return _0x3d1b();
-}
+        if (link.href.includes(`${activePage}`)) {
+            link.classList.add('active');
+        }
+    });
+});
